c06413c5599c194f9c192d1bddcfa7015ae2194d
Jerrox19 <eyerusalemgirmachew@gmail.com>
2022-09-13 01:54:13 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+""" contains file 2-square.py
+    defines the class square
+"""
+
+
+class Square:
+    """ a ckass Square that defines a square
+        Args:
+            __size (int) - size of square
+    """
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
