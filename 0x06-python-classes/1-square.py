4cd77ffb4da0b0c71c3b2457c46b477e0f11d9f4
Jerrox19 <eyerusalemgirmachew@gmail.com>
2022-09-13 01:44:20 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+""" contains 1-square.py
+    it contains the definition of class Square
+"""
+
+
+class Square:
+    """ defines a square
+        Args:
+            __size (int) - size of square
+    """
+    def __init__(self, size=0):
+        """instantiation of class with size attribute"""
+        self.__size = size
