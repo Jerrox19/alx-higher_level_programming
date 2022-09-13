29ba3ab20982eb3f135f8e871a36954d80a90bfc
Jerrox19 <eyerusalemgirmachew@gmail.com>
2022-09-13 02:12:22 -0700

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" contains file 2-square.py
+    defines the class square
+"""
+
+
+class Square:
+    """ a ckass Square that defines a square
+        Args:
+            __size (int) - size of square
+    """
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns the area of square"""
+        return self.__size ** 2
